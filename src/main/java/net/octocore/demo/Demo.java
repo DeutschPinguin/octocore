2bb168a7938be215b4f96aea6215a7d18564b9c2
DeutschPinguin <179315969+DeutschPinguin@users.noreply.github.com>
2026-07-04 01:43:33 +0300

    non-blocking packet receiving

--- src/main/java/net/octocore/demo/Demo.java
+++ src/main/java/net/octocore/demo/Demo.java
@@ -1,7 +1,13 @@
 package net.octocore.demo;
 
+import net.octocore.network.datatype.VarInt;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -14,27 +20,55 @@ public class Demo
 		try (var client = new Socket("localhost", 25565); var in = client.getInputStream(); var out = client.getOutputStream())
 		{
 			System.out.print("Connected to the server\n> ");
-			var scanner = new Scanner(System.in);
-			
-			while (true)
-			{
-				var data = scanner.nextLine();
-				
-				if (data.toLowerCase().startsWith("q")) break;
-				
-				try
-				{
-					int num = Integer.parseInt(data);
-					out.write(num);
-					System.out.printf("Sent to the server: %d\n> ", num);
-				}
-				catch (NumberFormatException e)
-				{
-					System.err.printf("Incorrect number format: '%s'\n> ", data);
-				}
-			}
+			demoFixedPacket(in, out, (byte) 0);
+			demoFixedPacket(in, out, (byte) 5);
+			out.write(0);
+			demoFixedPacket(in, out, (byte) 10);
+			demoFixedPacket(in, out, (byte) -1);
+			out.write(0);
+			out.write(0);
 		}
 		catch (IOException _) {}
+	}
+	
+	
+	private static void demoFixedPacket (InputStream in, OutputStream out, byte add)
+		throws IOException
+        {
+		var buf = ByteBuffer.allocate(5);
+		VarInt.write(buf, 4);
+		VarInt.write(buf, (byte) (20 + add));
+	        buf.put((byte) (10 + add));
+	        buf.put((byte) (50 + add));
+	        buf.put((byte) (100 + add));
+		var arr = buf.array();
+		out.write(arr);
+		System.out.printf("sent data (size=%d,id=%d) to server: %s\n", arr.length, arr[1], Arrays.toString(arr));
+	}
+	
+	
+	private static void demoScanner (InputStream in, OutputStream out)
+		throws IOException
+	{
+		var scanner = new Scanner(System.in);
+		
+		while (true)
+		{
+			var data = scanner.nextLine();
+			
+			if (data.toLowerCase().startsWith("q")) break;
+			
+			try
+			{
+				int num = Integer.parseInt(data);
+				out.write(num);
+				System.out.printf("Sent to the server: %d\n> ", num);
+			}
+			catch (NumberFormatException e)
+			{
+				System.err.printf("Incorrect number format: '%s'\n> ", data);
+			}
+		}
 	}
 
 }
--- src/main/java/net/octocore/network/ClientView.java
+++ src/main/java/net/octocore/network/ClientView.java
@@ -1,5 +1,6 @@
 package net.octocore.network;
 
+import net.octocore.network.datatype.VarInt;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,48 +10,73 @@ import java.nio.ByteBuffer;
 public final class ClientView
 {
         private static final Logger LOGGER = LogManager.getLogger("ClientView");
-        private ByteBuffer
-                packetBuffer = ByteBuffer.allocate(256),
-                workBuffer = ByteBuffer.allocate(256);
+        
+        private final ByteBuffer sizeBuffer = ByteBuffer.allocate(5);
+        private ByteBuffer packetBuffer, extraBuffer;
         private int packetSize = -1;
         
         
         /**
-         * @param buffer Buffer received from real client. Capcity is fixed, actual data size is varying but always contains at least one byte
+         * @param buffer Buffer with some chunk of the data received from real client
          */
-        public void receiveDataFromClient (final ByteBuffer buffer)
+        public void receiveDataFromClient (ByteBuffer buffer)
         {
-                /*
-                        TODO packets parsing
-                        TODO this.buffer overflow handling
-                        TODO remove all packet parsing and world updating from network thread
-                        TODO split client's update work (read VS write) into different threads
-                */
+                // TODO multithreading
                 
-//                this.packetBuffer.put(buffer);
-//
-//                if (this.packetSize == -1)
-//                {
-//                        while (this.packetBuffer.hasRemaining())
-//                        {
-//                               byte b = this.packetBuffer.get();
-//                               this.workBuffer.put(b);
-//                               int pos = this.workBuffer.position();
-//                               if (pos >= 4 || !VarInt.hasNextByte(b))
-//                               {
-//                                       this.packetSize = VarInt.read(this.workBuffer) + pos;
-//                                       this.workBuffer.clear();
-//                                       break;
-//                               }
-//                        }
-//                }
-//
-//                if (this.packetBuffer.limit() < this.packetSize) return;
+                if (this.extraBuffer != null && this.extraBuffer.hasRemaining())
+                {
+                        int capacity = this.extraBuffer.capacity() + buffer.capacity();
+                        buffer = ByteBuffer.allocate(capacity).put(this.extraBuffer).put(buffer).flip();
+                        this.extraBuffer = null;
+                }
+                
+                if (this.packetSize == -1)
+                {
+                        while (buffer.hasRemaining())
+                        {
+                               byte b = buffer.get();
+                               this.sizeBuffer.put(b);
+                               if (this.sizeBuffer.position() == 4 || !VarInt.hasNextByte(b))
+                               {
+                                       this.sizeBuffer.flip();
+                                       this.packetSize = VarInt.read(this.sizeBuffer);
+                                       this.sizeBuffer.clear();
+                                       if (this.packetSize < 1)
+                                       {
+                                               LOGGER.trace("Received packet with 0 size");
+                                               this.packetSize = -1;
+                                               continue;
+                                       }
+                                       this.packetBuffer = ByteBuffer.allocate(this.packetSize);
+                                       break;
+                               }
+                        }
+                        if (this.packetSize == -1) return;
+                }
+                
+                int sizeToLoad = this.packetBuffer.capacity() - this.packetBuffer.position();
+                if (sizeToLoad > 0)
+                {
+                        sizeToLoad = Math.min(sizeToLoad, buffer.remaining());
+                        var data = buffer.slice(buffer.position(), sizeToLoad);
+                        this.packetBuffer.put(data);
+                        buffer.position(buffer.position() + sizeToLoad);
+                }
+                
+                int maxPosition = this.packetBuffer.capacity() - 1;
+                if (this.packetBuffer.position() < maxPosition) return; // packet body isn't fully loaded
+                
+                if (buffer.hasRemaining()) this.extraBuffer = ByteBuffer.allocate(buffer.remaining()).put(buffer).flip();
+                this.packetBuffer.flip();
+                int id = VarInt.read(this.packetBuffer);
+                LOGGER.trace("Received packet (size {} | id {})", this.packetSize, id);
+                // TODO do something with fully loaded packet ...
+                this.packetSize = -1;
         }
         
         
         /**
-         * @param buffer Empty buffer which will be sent to real client after filling in this method
+         * @param buffer Empty buffer which will be sent to real client after filling there
          */
         public void sendDataToClient (final ByteBuffer buffer)
         {
